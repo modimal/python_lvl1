7b29c04f362cf131120c8eca9cd78261262c9750
Arsen Bekzhanov <liwidne@gmail.com>
2018-09-28 17:38:23 +0300

    ДЗ (7 урок)
    Сделал лото)

--- lesson07/loto.py
+++ lesson07/loto.py
@@ -10,6 +10,11 @@ class Ticket:
         self._row1, self._row2, self._row3 = [], [], []
         self._rows = {'row1': self._row1, 'row2': self._row2, 'row3': self._row3}
         self._new_ticket(self._rows)
+        self._formatted = self._format_ticket()
+
+    @property
+    def rows(self):
+        return self._rows
 
     def is_used(self, num):
         if num in self._row1:
@@ -24,7 +29,7 @@ class Ticket:
         for k, row in rows.items():
             i = 0
             while i < 5:
-                num = random.randrange(FROM_N, TO_N, 1)
+                num = random.randrange(FROM_N, TO_N)
                 if not self.is_used(num):
                     row.append(num)
                     i += 1
@@ -33,10 +38,12 @@ class Ticket:
     def crossing(self, num):
         row_n = 'row' + str(self.is_used(num))
         row = self._rows[row_n]
-        i = row.index(num)
-        row[i] = ' -'
+        row.remove(row[row.index(num)])
+        num = str(num)
+        num = ' ' + num + ' ' if len(num) == 1 else num
+        self._formatted = self._formatted.replace(num if len(num) == 3 else num, ' - ')
 
-    def __str__(self):
+    def _format_ticket(self):
         row_str = ''
         for k, row in self._rows.items():
             space, i, lenr = 0, 0, len(row)
@@ -49,12 +56,71 @@ class Ticket:
                     x = str(row[i])
                     row_str += x + ' ' if len(x) == 2 else ' ' + x + ' '
                     i += 1
-            if space < lenr - 2:
+            if space < lenr - 1:
                 row_str += '   ' * (lenr - 1 - space)
 
             row_str += '\n'
         return row_str
 
+    def __str__(self):
+        return self._formatted
 
-ticket = Ticket()
-print(ticket)
+
+class UserTicket(Ticket):
+    def __str__(self):
+        return '------ Ваша карточка -----\n'\
+               + super().__str__() + \
+               '--------------------------'
+
+
+class CompTicket(Ticket):
+    def __str__(self):
+        return '-- Карточка компьютера ---\n' \
+               + super().__str__() + \
+               '--------------------------'
+
+
+user_ticket = UserTicket()
+comp_ticket = CompTicket()
+kegs = list(range(FROM_N, TO_N))
+
+print('\n{}\n\n{}'.format(user_ticket, comp_ticket))
+
+while True:
+    is_user_winner = all(len(v) == 0 for v in user_ticket.rows.values())
+    is_comp_winner = all(len(v) == 0 for v in comp_ticket.rows.values())
+
+    if is_user_winner and is_comp_winner:
+        print('Ничья!')
+        exit(0)
+    elif is_user_winner:
+        print('Поздравляю! Вы победили!')
+        exit(0)
+    elif is_comp_winner:
+        print('В этот раз выиграл компьютер.')
+        exit(0)
+
+    random_keg_i = random.randrange(len(kegs))
+
+    print('\nНовый бочонок: {} (осталось {})\n{}\n\n{}'
+          .format(kegs[random_keg_i], len(kegs) - 1, user_ticket, comp_ticket))
+
+    answer = input('Зачеркнуть цифру? (y/n)\n').strip().lower()
+
+    if comp_ticket.is_used(kegs[random_keg_i]):
+        comp_ticket.crossing(kegs[random_keg_i])
+
+    if answer == 'y':
+        if user_ticket.is_used(kegs[random_keg_i]):
+            user_ticket.crossing(kegs[random_keg_i])
+        else:
+            print('У Вас нет такой цифры. Вы проиграли.')
+            exit(0)
+    elif answer == 'n':
+        if user_ticket.is_used(kegs[random_keg_i]):
+            print('У Вас есть такая цифра. Вы проиграли.')
+            exit(0)
+    else:
+        raise ValueError('вводите только допустимые символы (y/n)')
+
+    kegs.remove(kegs[random_keg_i])
